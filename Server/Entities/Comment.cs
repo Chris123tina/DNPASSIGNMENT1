018f566d2289fc1531269123f6a7450fc7d91bcb
Chris123tina <christinajacob347@gmail.com>
2026-09-02 17:31:13 +0200

    entities files code

---
+++ Server/Entities/Comment.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities;
+
+public class Comment
+{
+    public string body { get; set; }
+    public int userId { get; set; }
+    public int id { get; set; }
+    public int postId { get; set; }
+}
